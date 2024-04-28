fac198c1ceebfb2a59a75d4b0e6576674b6ac82a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-28 11:36:08 +0000

    Updating to latest FlutterFlow output.

--- android/app/src/main/kotlin/com/example/qi_bus_prokit/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.qi_bus_prokit
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/codePicker/country_codes.dart
+++
@@ -1,4 +0,0 @@
-List<Map> codes = [
-  {"name": "المغرب", "code": "MA", "dial_code": "+212"},
-  {"name": "United States", "code": "US", "dial_code": "+1"},
-];
